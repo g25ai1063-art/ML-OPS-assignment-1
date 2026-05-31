cdd4e703b910f095b08aeff614212a2d638f651b
soumyashanta <g25ai1063@iitj.ac.in>
2026-05-31 17:54:38 +0000

    Add Kernel Ridge implementation

---
+++ train2.py
@@ -0,0 +1,27 @@
+from sklearn.kernel_ridge import KernelRidge
+
+from misc import (
+    load_data,
+    preprocess_data,
+    train_model,
+    evaluate_model
+)
+
+df = load_data()
+X_train, X_test, y_train, y_test = preprocess_data(df)
+
+model = KernelRidge()
+
+model = train_model(
+    model,
+    X_train,
+    y_train
+)
+
+mse = evaluate_model(
+    model,
+    X_test,
+    y_test
+)
+
+print(f"Kernel Ridge MSE: {mse}")
